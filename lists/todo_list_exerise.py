54f867420f4e1ade5fb42e43609d9f73953f99de
Olusola Adekunle <olusolaadek@hotmail.com>
2022-11-25 16:49:40 +0100

    copy list with copy() and copy.deepcopy()

---
+++ lists/todo_list_exerise.py
@@ -0,0 +1,12 @@
+"""
+# Todo List Exercise
+
+Create an interactive, text-based todo list that has the following features:
+
+- A user can add todos by entering them into the prompt
+- A user can complete todos by entering the todo’s corresponding number
+- A user can view a help screen by typing ‘h’ or ‘help’
+- A user can quit by typing ‘q’ or ‘quit
+"""
+
+print('====== Welcome to My TODO List =====')
